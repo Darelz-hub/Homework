505b750fe50b3b247106e9a3bcbeb74b0b30a2d9
Darelz-hub <139675326+Darelz-hub@users.noreply.github.com>
2023-08-20 19:41:36 +0300

    Add files via upload

---
+++ Homework/homework1 tower/play.py
@@ -0,0 +1,51 @@
+class Tower:
+    def __init__(self,hp: int  = 100, cp: int = 100):
+        self.__hp = hp
+        self.__cp = cp
+        self.__doing = 'Стоит'
+    
+    
+    def show(self):
+        print ( self.__hp, self.__cp, self.__doing)
+    
+    @property
+    
+    def hp(self):
+        return self.__hp
+    
+    @property
+    
+    def cp(self):
+        return self.__cp
+    @property
+    
+    def doing(self):
+        return self.__doing
+    
+    def __sub__(self, n):
+        cp = n
+        hp = cp/2
+        self.__hp -= hp 
+        self.__cp -= cp
+        #return self
+        
+class ShootTower(Tower):
+    
+    def __init__(self, hp: int = 70, cp: int = 50):
+        super().__init__(hp,cp)
+        self.__action = 'Стрелять'
+    
+    def show(self):
+        print( self.hp,self.cp,self.doing, self.__action )
+        
+tower = Tower()
+tower.show()
+n = int(input('Введите урон'))
+tower - n
+tower.show()
+tower1 = ShootTower()
+n = int(input('Введите урон'))
+tower1 - n
+tower1.show()
+
+
