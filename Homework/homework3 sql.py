463354afccc67a26487427b3d364ebdb0ace4a02
Darelz-hub <139675326+Darelz-hub@users.noreply.github.com>
2023-09-24 21:28:11 +0300

    Update homework3 sql.py

--- Homework/homework3 sql.py
+++ Homework/homework3 sql.py
@@ -13,8 +13,7 @@
 # # city (город)
 # # rating (рейтинг)
 # # id_sp(номер продавца, который обслуживает этого заказчика)*/
-# # и вставить в них данные из файлов
-# # https://github.com/makarova1507ana/python323/tree/main/SQL/%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D0%B5%20%D1%84%D0%B0%D0%B9%D0%BB%D1%8B 
+# Реализовать интерфейс для регистрации, редактирование и удаления записей о продавцах и заказчиков 
 import sqlite3
 import random 
 Salespeople = []
@@ -133,59 +132,6 @@ with sqlite3.connect('shops.db') as con:
                     FOREIGN KEY(id_sp) references salespeople(id)
                 );
                 ''')
-    # print(Salespeople)
-    # for el in Salespeople:
-    #     cur.execute('insert into salespeople values (null,?,?,?)', (el[0], el[1], el[2]))
-    #         #cur.execute('insert into salespeople nametable(****) values (sname,city,comm)', (line[0], line[1], line[2]))
-    # for el in Customer:
-    #     cur.execute('insert into customers values (null,?,?,?,?)',(el[0], el[1], el[2], el[3]))
-            
-    
-    # with open('customer.txt', 'r') as f:
-    #     for line in f.readlines():
-    #         line = line.split()
-    #         cur.execute('insert into customers values (cname,city,rating,id_sp)', (line[0],line[1],line[2],line[3]))
-    # result = cur.execute("""
-    #             SELECT * FROM salespeople,customers where customers.id_sp = salespeople.id and customers.city Like'%Москва'
-    #             """) 
-    # for row in result.fetchall():
-    #     print(row)
-
-    # name_user = 'salespeople123'
-    # result = cur.execute(f"""
-    #             SELECT name_user FROM salespeople WHERE name_user = \'{name_user}\'
-    #             """) 
-    # #print(result.fetchall())
-    
-    # while len(result.fetchall()) != 0:
-    #     name_user = 'salespeople' + str(random.randint(1,1000))
-    #     # проверка наличия пользователя
-    #     result = cur.execute(f"""
-    #         SELECT name_user FROM salespeople WHERE name_user = \'{name_user}\'
-    #             """)
-        
-    # passorwd = name_user
-    # cur.execute('insert into Salespeople(name_user, password,sname) values (?,?,?)',(name_user, passorwd, 'Михаил Петрович'))
-    # result = cur.execute(f"""
-    #         SELECT * FROM salespeople
-    #         """)
-    # for row in result.fetchall():
-    #     print(row)
-    
-    # name_user_c = 'customer321'
-    # result = cur.execute(f''' select name_user_c from customers where name_user_c = \'{name_user_c}\'''')
-    # while len(result.fetchall()) != 0:
-    #     name_user_c = 'custoner' + str(random.randint(1,1000))
-    #     result = cur.execute(f'''select name_user_c from customers where name_user_c = \' {name_user_c}\'''')
-        
-    # passorwd_c = name_user_c
-    
-    # cur.execute('insert into customers(name_user_c, password_c,cname) values (?,?,?)', (name_user_c, passorwd_c,'Иван Петрович '))
-    # result = cur.execute(f"""
-    #         SELECT * FROM customers
-    #         """)
-    # for row in result.fetchall():
-    #     print(row)
 
     while True:
             print('''Выберите таблицу для взаимодействия:
